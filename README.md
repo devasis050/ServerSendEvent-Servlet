50d2722ab9068345555efe3513a6cef855049e57
devasis <devasis.majhi@gmail.com>
2018-09-23 15:07:36 +0100

    Initital commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ServerSendEvent-Servlet
+
+This is a sample app with servlet. This is just to understand basic working of server sent event. 
+In real time we will use either Spring Web Flux or web socket. 
+
+Things not taken care 
+1) Client cors error. Working in browser currently.
+2) Not effor has been put to understand AsyncContext. 
+3) This app is not asynchronous.
+
+
---
+++ server-send-event/src/main/java/com/example/NotificationServlet.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.io.IOException;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/test"}, asyncSupported = true)
+public class NotificationServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/event-stream");
+		AsyncContext actx = req.startAsync(req,res);
+		//Use the context to use it asynchronously. We may look into listener for better control./
+		actx.setTimeout(10 * 1000);
+		
+		actx.getResponse().getWriter().println("First Response");
+		actx.getResponse().getWriter().flush();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		actx.getResponse().getWriter().println("Second Response");
+		actx.complete();
+	}
+
+}
